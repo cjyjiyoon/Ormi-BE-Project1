e33590d0c44e86975b564a3b5267e73f6d12f51c
cjy <cjiyoon1130@gmail.com>
2024-08-27 13:45:20 +0900

    Refactor : html 통합, css 파일 분리, js 코드 리팩터

--- script.js
+++ script.js
@@ -3,22 +3,51 @@ const nav = document.querySelector('.nav_top');
 const header = document.querySelector('.header');
 const headerRect = header.getBoundingClientRect();
 const modal = document.querySelector('.modal_layer');
-const submitModalButton = document.querySelector('.btn_modal_submit');
-const subBtn = document.querySelectorAll('.subscribe1, .subscribe2');
-const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-].[A-Za-z0-9\-]+/;
-const slideMenu = document.querySelector('.slide');
+const subscribeButtons = document.querySelectorAll('.subscribe1, .subscribe2');
 const slideMenuButton = document.querySelector('.button_menu');
 const slideBackButton = document.querySelector('.button_back');
 
 let emailInput = document.querySelector('.email_text');
 
-// 스크롤 관련 기능
-// function scrollEvent(){
-//     const 
-// }
+// 함수
+// 이메일 유효성 체크
+function emailValidCheck(email) {
+    const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-].[A-Za-z0-9\-]+/;
+    return pattern.test(email.value);
+}
+
+// modal 팝업
+function loadModal() {
+    modal.style.display = 'flex';
+    const submitModalButton = document.querySelector('.btn_modal_submit');
+
+    submitModalButton.addEventListener('click', () => {
+        postEmail(emailInput.value);
+        modal.style.display = 'none';
+    });
+}
+
+// 데이터 전송
+function postEmail(data) {
+    fetch("#")
+        .then((data) => data.json())
+        .then((data) => {
+            console.log(data);
+            return data[0];
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+}
+
+// 슬라이드 메뉴 토글
+function toggleSlide() {
+    const slideMenu = document.querySelector('.slide');
+    slideMenu.classList.toggle('open');
+}
 
 // 스크롤 관련 기능(버튼 등장, 내비바 고정)
-window.addEventListener('scroll', function () {
+function scrollEvent() {
     if (window.scrollY > 500) {
         if ((document.documentElement.scrollWidth > 768)) {
             topBtn.style.display = 'block';
@@ -28,68 +57,26 @@ window.addEventListener('scroll', function () {
         topBtn.style.display = 'none';
         nav.style.position = 'absolute';
     }
-});
-// 스크롤 탑 버튼 기능
-topBtn.addEventListener('click', function () {
+}
+
+// 최상단으로 스크롤 이동
+function scrollTop() {
     window.scrollTo({ top: 0, behavior: "smooth" })
-});
+}
 
-
-
-// 이메일 유효성 검사 
-function emailValidCheck(email) {
-    if (pattern.test(email.value) === false) {
-        return false;
-    } else {
-        return true;
+// 모달 버튼 클릭 (이메일 검사 포함)
+function subscribeModal() {
+    if (emailValidCheck(emailInput)) {
+        loadModal();
+    }
+    else {
+        alert('이메일 형식이 유효하지 않습니다.');
     }
 }
 
-// modal 팝업
-function loadModal() {
-    modal.style.display = 'flex';
-    submitModalButton.addEventListener('click', () => {
-        postEmail(emailInput.value);
-        modal.style.display = 'none';
-    });
-}
-
-
-
-// 데이터 전송
-function postEmail(data){
-    fetch("#")
-    .then((data)=>data.json())
-    .then((data)=>{
-        console.log(data);
-        return data[0];
-    })
-    .catch((error)=>{
-        console.log(error);
-    })
-        
-}
-
-// 모달 버튼
-subBtn.forEach(button => {
-    button.addEventListener('click', function () {
-        if (emailValidCheck(emailInput) === true) {
-            loadModal();
-
-        }
-        else {
-            alert('이메일 형식이 유효하지 않습니다.');
-        }
-    })
-});
-
-// 슬라이드 메뉴 표시
-slideMenuButton.addEventListener('click', () => {
-    slideMenu.classList.toggle('open');
-});
-// 슬라이드 메뉴 비표시
-slideBackButton.addEventListener('click', () => {
-    slideMenu.classList.toggle('open');
-})
-
-
+//이벤트 리스너
+slideMenuButton.addEventListener('click', toggleSlide);
+slideBackButton.addEventListener('click', toggleSlide);
+window.addEventListener('scroll', scrollEvent);
+topBtn.addEventListener('click', scrollTop);
+subscribeButtons.forEach(button => button.addEventListener('click', subscribeModal));
